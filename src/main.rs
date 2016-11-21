b6a2d97c93efc221ea3ab3913d8d08da0c8a7db4
Mauricio Rivera <andymaster01@gmail.com>
2016-11-21 18:07:58 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+extern crate sfml;
+
+use sfml::window::{VideoMode, Event, window_style, ContextSettings, Key};
+use sfml::graphics::{RenderWindow};
+
+fn main() {
+
+    let mut window = RenderWindow::new(VideoMode::new_init(800, 600, 32),
+						"The Game!",
+						window_style::CLOSE,
+						&ContextSettings::default()
+			).unwrap();
+
+    loop {
+
+	for event in window.events(){
+		match event {
+			Event::KeyPressed { code, .. } => {
+				match code {
+					Key::Escape => return,
+					_ => {}
+				}
+			},
+		  _ => {}
+		}
+	}
+
+ 	window.display();
+    }
+
+    
+
+}
