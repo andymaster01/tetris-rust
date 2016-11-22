91bebb1aedd0ddd4ffb4f5503baa5cb6ce213456
Mauricio Rivera <andymaster01@gmail.com>
2016-11-21 19:03:07 -0500

    Show only a portion of the texture

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate sfml;
 
 use sfml::window::{VideoMode, Event, window_style, ContextSettings, Key};
-use sfml::graphics::{RenderWindow, Color, RenderTarget, Texture, Sprite};
+use sfml::graphics::{RenderWindow, Color, RenderTarget, Texture, Sprite, IntRect};
 
 fn main() {
 
@@ -17,6 +17,10 @@ fn main() {
 
         let tiles = Texture::from_file("tiles.png").unwrap();
         let mut image = Sprite::with_texture(&tiles);
+
+        let rect = IntRect{top:0, left:0, height:18, width:18};
+
+        image.set_texture_rect(&rect);
 
 	    for event in window.events(){
 		    match event {
